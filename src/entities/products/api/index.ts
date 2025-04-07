adad0ba3c2aecb55924b2a0f787c76177cae7732
Daniil <baldindaniilza@mail.ru>
2025-04-07 22:29:55 +0500

    Сервер перенесен в отдельный проект

--- src/entities/products/api/index.ts
+++ src/entities/products/api/index.ts
@@ -1,14 +1,9 @@
 import { api, API_ENDPOINTS } from "@/shared/api";
 import { Product } from "../types";
-import axios from "axios";
 
 export const getProducts = async (): Promise<Product[]> => {
 	try {
-		const response = await axios.get("/api/products", {
-			headers: {
-				"Access-Control-Allow-Origin": "http://localhost:3000",
-			},
-		});
+		const response = await api.get(API_ENDPOINTS.products);
 		return response.data;
 	} catch (err) {
 		console.log("Ошибка при получении продуктов: " + err);
